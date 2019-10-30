def6878d2c3769d375eb719cf99b951f0d5fb5f1
Danboards <1066673870@qq.com>
2019-10-30 23:59:25 +0800

    增加base包

--- src/main/java/com/xyz/base/StringNote.java
+++ src/main/java/com/xyz/base/StringNote.java
@@ -1,4 +1,13 @@
 package com.xyz.base;
 
 public class StringNote {
+
+
+    public static void main(String args[]) {
+        String str = "小学,初中,高中,大专,本科,研究生,博士,";
+        String[] buff = str.split(",");
+        for(int i=0;i<buff.length;i++){
+            System.out.println(buff[i]);
+        }
+    }
 }
